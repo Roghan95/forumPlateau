14921b8ad064dc75a65c9af01e22f8bdc6895bb8
Roghan95 <p90svborz@gmail.com>
2023-09-06 16:45:56 +0200

    Correction erreur de syntaxe

--- view/forum/PostsByTopics.php
+++ view/forum/PostsByTopics.php
@@ -12,33 +12,29 @@ $topic = $result["data"]['topic'];
 
 <div class="posts-container">
     <?php
-    if ($posts) {
-        foreach ($posts as $post) { ?>
-            <div class="post-block">
-                <div class="post-info">
-                    <figure>
-                        <img src="https://picsum.photos/50/50" alt="Photo de profil">
-                        <!-- On affiche le pseudo de l'utilisateur qui a créer le post -->
-                        <p><?= $post->getUser() ?></p>
-                    </figure>
-                    <div class="user-date">
-                        <!-- On affiche la date de création du post -->
-                        <p>Le <?= $post->getDateCreation() ?></p>
-                    </div>
+    // if ($posts) {
+    foreach ($posts as $post) { ?>
+        <div class="post-block">
+            <div class="post-info">
+                <figure>
+                    <img src="https://picsum.photos/50/50" alt="Photo de profil">
+                    <!-- On affiche le pseudo de l'utilisateur qui a créer le post -->
+                    <p><?= $post->getUser() ?></p>
+                </figure>
+                <div class="user-date">
+                    <!-- On affiche la date de création du post -->
+                    <p>Le <?= $post->getDateCreation() ?></p>
                 </div>
-                <p>
-                    <!-- On affiche le texte du post -->
-                    <?= $post->getTexte() ?>
-                </p>
             </div>
-    <?php }
-    } else {
-        echo "<p>Pas de posts dans ce topic</p>";
-    }
-    ?>
-    <form class="reponse-form" action="index.php?ctrl=forum&action=addPost&id=<?= $topics->getId() ?>" method="post">
+            <p>
+                <!-- On affiche le texte du post -->
+                <?= $post->getTexte() ?>
+            </p>
+        </div>
+    <?php } ?>
+    <form class="reponse-form" action="index.php?ctrl=forum&action=addPost&id=<?= $topic->getId() ?>" method="post">
         <label for="message-textarea">Répondre: </label>
         <textarea id="message-textarea" name="texte" required></textarea>
-        <input type="submit" name="addPost">POSTER</input>
+        <input type="submit" name="addPost" value="POSTER">
     </form>
 </div>
